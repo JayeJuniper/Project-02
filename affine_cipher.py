71e97f0a8fc269d4850b491e24800c73520e6ad3
JonathanGrenz <32690541+JonathanGrenz@users.noreply.github.com>
2017-11-13 15:07:42 -0800

    Update affine_cipher.py
    I Fixed the pip8 abuses and restructured encrypt/decrypt functions using simpler code and thus removing a redundancy. As i was unable to replicate the crash i was told would happen, i hope this new code fixes that issue. This cipher code works as intended on my machine.

--- affine_cipher.py
+++ affine_cipher.py
@@ -4,16 +4,15 @@ from ciphers import Cipher
 
 
 class Affine(Cipher):
-    """The affine cipher is a type of monoalphabetic substitution 
+    """The affine cipher is a type of monoalphabetic substitution
     cipher, wherein each letter in an alphabet is mapped to its numeric
-    equivalent, encrypted using a simple mathematical function, and 
-    converted back to a letter. Each letter is enciphered with the 
+    equivalent, encrypted using a simple mathematical function, and
+    converted back to a letter. Each letter is enciphered with the
     function (ax + b) mod 26, where b is the magnitude of the shift.
     """
 
-
-    #defines initial aplphabet
     def __init__(self):
+        """Defines initial aplphabet."""
         self.alphabet = string.ascii_uppercase
         self.mod26 = {letter: number for letter,
                       number in zip([item for item in self.alphabet],
@@ -21,35 +20,20 @@ class Affine(Cipher):
         self.mod26_inverse = {number: letter for number,
                               letter in zip([num for num in range(0, 26)],
                                             [item for item in self.alphabet])}
-            
-    # encryption function E(x)= (ax+b)mod26
+
     def encrypt(self):
+        """encryption function E(x)= (ax+b)mod26"""
         text = input("What would you like to encrypt?\n>> ").upper()
-        proxy_text = [letter for letter in text]
-        encrypted_text = []
-        
-        for letter in proxy_text:
-            if letter == ' ':
-                encrypted_text.append('')
-            else:
-                encrypted_text.append(str(self.mod26[letter]))
-                
+        proxy_text = [letter for letter in text.replace(" ", "")]
+        encrypted_text = [str(self.mod26[letter]) for letter in proxy_text]
+
         return ' '.join(encrypted_text)
-        
-        
-    #decryption function D(x) = a^(-1)(x-b)mod26
+
     def decrypt(self):
-        text = input("""What would you like to decrypt?\nBe sure to separate \
-numbers with a space!\n>> """
-                    ).upper()
-        proxy_text = [item for item in text.split(' ')]
-        decrypted_text = []
-        
-        for number in proxy_text:
-            if number == ' ':
-                decrypted_text.append('')
-            else:
-                decrypted_text.append(self.mod26_inverse[int(number)])
-            
+        """decryption function D(x) = a^(-1)(x-b)mod26"""
+        text = input("What would you like to decrypt?\nBe sure to separate nu\
+mbers with a space!\n>> ").upper()
+        proxy_text = [int(item) for item in text.split(' ')]
+        decrypted_text = [self.mod26_inverse[number] for number in proxy_text]
+
         return ''.join(decrypted_text)
-        
